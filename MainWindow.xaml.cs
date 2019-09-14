e0713f1591ee593cd0724998ec011207a4ca432b
Kirtle <xkirtle26@gmail.com>
2019-09-14 20:19:04 +0100

    Initial Structure of the program
    Parsed some random data from an online API to provide me details to create accounts with.
    
    Next, I need to fill the register fields with some DOM elements, most likely.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace BulkRegister
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Generate_Click(object sender, RoutedEventArgs e)
+        {
+            System.Net.ServicePointManager.ServerCertificateValidationCallback = delegate { return true; }; //Simple "catch all" for the ssl/tls trust relationship errors
+
+            using (WebClient client = new WebClient())
+            {
+                //Parsing the data we need from the api
+                var json = client.DownloadString("https://api.namefake.com/portuguese-brazil/random/");
+                JObject o = JObject.Parse(json);
+                var name = (string)o["username"];
+                var email_u = (string)o["email_u"];
+                var email_d = (string)o["email_d"];
+                var password = (string)o["password"];
+                //Adding it to our text boxes
+                Name.Text = name;
+                Email.Text = email_u + "@" + email_d;
+                Password.Text = password;
+            }
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

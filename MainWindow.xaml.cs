65b7b9906f66939339602b101905062b9da463c4
Kirtle <xkirtle26@gmail.com>
2019-09-16 01:07:24 +0100

    Changed the design and added some features
    Improved the main structure of the app (xaml design), added a button that registers the user with the generated data and am midway through of saving every registered account in a listbox.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,45 @@
 ﻿using System.Windows;
 using Newtonsoft.Json.Linq;
 using System.Net;
+using CefSharp;
+using CefSharp.Wpf;
+using WindowsInput;
+using System.Collections.Generic;
 
 namespace BulkRegister
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+    public class Accounts
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+
     public partial class MainWindow : Window
     {
+        InputSimulator inp = new InputSimulator();
+        ChromiumWebBrowser chrome = new ChromiumWebBrowser();
+
+        List<Accounts> Account = new List<Accounts>() { };
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //comboBox.ItemsSource = Account;
+            //listBox.ItemsSource = Account;
+            foreach (Accounts c in Account)
+            {
+                comboBox.Items.Add(Account);
+                listBox.Items.Add(Account);
+            }
         }
 
         private void Generate_Click(object sender, RoutedEventArgs e)
@@ -36,7 +64,40 @@ namespace BulkRegister
 
         private void Register_Click(object sender, RoutedEventArgs e)
         {
+            //string script = "document.getElementById('username')[0].value = '" + Name.Text + "';";
+            //chrome.ExecuteScriptAsync(script);
+            //chrome.GetMainFrame().ExecuteJavaScriptAsync(script);
 
+            //Couldn't solve this with DOM properties, yet
+
+            Account.Add(new Accounts() { Name = Name.Text, Password = Password.Text });
+
+            for (int i = 0; i < 11; i++)
+            {
+                inp.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.TAB); //Leaves the input at the username
+            }
+
+            //Ciruclates through all of the other fields
+            inp.Keyboard.TextEntry(Name.Text);
+            inp.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.TAB);
+            inp.Keyboard.TextEntry(Email.Text);
+            inp.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.TAB);
+            inp.Keyboard.TextEntry(Password.Text);
+            inp.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.TAB);
+            inp.Keyboard.TextEntry(Password.Text);
+            inp.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.TAB);
+            inp.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.SPACE);
+            inp.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.TAB);
+            inp.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.SPACE); //Registers 
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e) //Saving all the accounts details that were generated
+        {
+            Account.Add(new Accounts() { Name = Name.Text, Password = Password.Text }); //Not quite working
+
+            //Can't read the values on the list since they show blank, yet a new index is generated
+            comboBox.Items.Add(Name.Text);
+            listBox.Items.Add(Password.Text);
         }
     }
 }
